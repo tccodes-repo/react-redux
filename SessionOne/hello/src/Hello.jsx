bd4d76f92386b370308169cf9282095c3da24a30
Derek Smith <derek.smith@naveego.com>
2019-09-09 20:42:25 -0400

    Renaming stuff

---
+++ SessionOne/hello/src/Hello.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+class Hello extends React.Component {
+  
+  render() {
+
+    return (
+
+      <div className="container">
+
+        <h1>Hello World!</h1>
+
+      </div>
+
+    );
+
+  }
+  
+}
+
+export default Hello;
